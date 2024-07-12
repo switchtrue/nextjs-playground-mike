0c94881c01c44184dfce8d1f65c0efb2c10ccefa
Mike Leonard <mike.leonard@petcircle.com.au>
2024-07-13 09:00:01 +1000

    Added link on the home page to the Buikder demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,6 +44,20 @@ export default function Home() {
             loading state
           </p>
         </li>
+        <li className="border-b-2 p-6">
+          <Link href="/builder" className="underline">
+            /builder
+          </Link>
+          <p>
+            Integrated with Builder.io do demonstrate and ideal e-commerce
+            experience. The core page and product information is a client
+            component fully rendered server side. Loading skeletons for
+            recommended products and customer reviews are also server side
+            rendered and then the actual data is fetched clientside. All three
+            core components are registered with Builder and fully editable using
+            the Builder visual editor.
+          </p>
+        </li>
       </ul>
     </main>
   );
