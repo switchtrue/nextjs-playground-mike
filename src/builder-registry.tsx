98a332b50e0120ea55a88078b54c7dc475ffbab1
Chris Campbell <chris.campbell@petcircle.com.au>
2024-07-12 18:54:35 +0800

    change to dynamic import components

--- src/builder-registry.tsx
+++ src/builder-registry.tsx
@@ -1,26 +1,43 @@
+"use client";
+
 import dynamic from "next/dynamic";
 import { type RegisteredComponent } from "@builder.io/sdk-react";
-import { ProductInformation } from "./components/productinformation";
-import { CustomerReviewsWithClientFetching } from "./components/customerreviewswithclientfetching";
-import { RecommendationsWithClientFetching } from "./components/recommendationswithclientfetching";
+// import { ProductInformation } from "./components/productinformation";
+// import { CustomerReviewsWithClientFetching } from "./components/customerreviewswithclientfetching";
+// import { RecommendationsWithClientFetching } from "./components/recommendationswithclientfetching";
 
 export const customComponents: RegisteredComponent[] = [
   {
-    component: ProductInformation,
+    component: dynamic(() =>
+      import("./components/productinformation").then(
+        (mod) => mod.ProductInformation
+      )
+    ),
+    // component: ProductInformation,
     name: "Product Information",
     inputs: [
       {
         name: "product",
-        type: "string",
+        type: "object",
       },
     ],
   },
   {
-    component: CustomerReviewsWithClientFetching,
+    component: dynamic(() =>
+      import("./components/customerreviewswithclientfetching").then(
+        (mod) => mod.CustomerReviewsWithClientFetching
+      )
+    ),
+    // component: CustomerReviewsWithClientFetching,
     name: "Customer Reviews",
   },
   {
-    component: RecommendationsWithClientFetching,
+    component: dynamic(() =>
+      import("./components/recommendationswithclientfetching").then(
+        (mod) => mod.RecommendationsWithClientFetching
+      )
+    ),
+    // component: RecommendationsWithClientFetching,
     name: "Recommendations",
   },
 ];
